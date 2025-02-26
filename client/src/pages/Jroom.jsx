aee267b2cd7c5f86f318ad6f5b17eb8ed082863f
Aman Thakur <79102837+AMAN-THAKUR1@users.noreply.github.com>
2025-02-27 02:05:00 +0530

    Update Jroom.jsx

--- client/src/pages/Jroom.jsx
+++ client/src/pages/Jroom.jsx
@@ -12,7 +12,7 @@ function Jroom() {
     const handleclick = async (e) => {
         e.preventDefault();
         try {
-            const response = await fetch("http://localhost:5000/join", {
+            const response = await fetch("https://anonyme-kz4t.onrender.com/join", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
